e427d49f50a942e387db50a33f87223840d82cb5
jalaimo <julien.alaimo@gmail.com>
2017-09-20 11:18:50 +0200

    add footer

---
+++ js/footer.js
@@ -0,0 +1,30 @@
+var div = document.createElement("div");
+
+div.innerHTML =
+    "<footer class=\"page-footer\">\n" +
+    "          <div class=\"container\">\n" +
+    "            <div class=\"row\">\n" +
+    "              <div class=\"col l6 s12\">\n" +
+    "                <h5 class=\"white-text\">Footer Content</h5>\n" +
+    "                <p class=\"grey-text text-lighten-4\">You can use rows and columns here to organize your footer content.</p>\n" +
+    "              </div>\n" +
+    "              <div class=\"col l4 offset-l2 s12\">\n" +
+    "                <h5 class=\"white-text\">Links</h5>\n" +
+    "                <ul>\n" +
+    "                  <li><a class=\"grey-text text-lighten-3\" href=\"#!\">Link 1</a></li>\n" +
+    "                  <li><a class=\"grey-text text-lighten-3\" href=\"#!\">Link 2</a></li>\n" +
+    "                  <li><a class=\"grey-text text-lighten-3\" href=\"#!\">Link 3</a></li>\n" +
+    "                  <li><a class=\"grey-text text-lighten-3\" href=\"#!\">Link 4</a></li>\n" +
+    "                </ul>\n" +
+    "              </div>\n" +
+    "            </div>\n" +
+    "          </div>\n" +
+    "          <div class=\"footer-copyright\">\n" +
+    "            <div class=\"container\">\n" +
+    "            © 2014 Copyright Text\n" +
+    "            <a class=\"grey-text text-lighten-4 right\" href=\"#!\">More Links</a>\n" +
+    "            </div>\n" +
+    "          </div>\n" +
+    "        </footer>"
+
+document.body.appendChild(div);
